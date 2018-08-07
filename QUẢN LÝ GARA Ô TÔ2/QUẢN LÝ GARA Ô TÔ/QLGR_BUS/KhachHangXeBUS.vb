e6098685092c560a2e8fabb7c0501af261835e06
VÕ THỊ MỘT <16520756@ms.uit.edu.vn>
2018-08-07 21:47:10 +0700

    new commit from vscode

---
+++ QUẢN LÝ GARA Ô TÔ2/QUẢN LÝ GARA Ô TÔ/QLGR_BUS/KhachHangXeBUS.vb
@@ -0,0 +1,57 @@
+﻿Imports QLGR_DAL
+Imports QLGR_DTO
+Imports Utility
+
+Public Class KhachHangXeBUS
+
+    Public Function isValiIDKH(KhachHangXe_DTO As KhachHangXeDTO) As Boolean
+
+        If (KhachHangXe_DTO.MaKH1.Length < 1) Then
+            Return False
+        End If
+
+        Return True
+
+    End Function
+
+    Public Function isValidName(KhachHangXe_DTO As KhachHangXeDTO) As Boolean
+
+        If (KhachHangXe_DTO.HoTen1.Length < 1) Then
+            Return False
+        End If
+
+        Return True
+
+    End Function
+
+    Public Function isValidAddress(KhachHangXe_DTO As KhachHangXeDTO) As Boolean
+
+        If (KhachHangXe_DTO.DiaChi1.Length < 1) Then
+            Return False
+        End If
+
+        Return True
+
+    End Function
+
+    Public Function isValidPhone(KhachHangXe_DTO As KhachHangXeDTO) As Boolean
+
+        If (KhachHangXe_DTO.SoDienThoai1.Length < 1) Then
+            Return False
+        End If
+
+        Return True
+
+    End Function
+
+    Public Function isValidThebalance(KhachHangXe_DTO As KhachHangXeDTO) As Boolean
+
+        If (KhachHangXe_DTO.SoTienNo1.ToString().Length < 1) Then
+            Return False
+        End If
+
+        Return True
+
+    End Function
+
+End Class
